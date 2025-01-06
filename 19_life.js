e429a54df7aebeb164e758a1054a2cc82dce825b
Code-By-Tom2 <shovanchaudhauri2019@gmail.com>
2025-01-06 23:42:37 +0530

    more basices of js

---
+++ 19_life.js
@@ -0,0 +1,12 @@
+// immediatly invoke function expressions(IIFE)
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+}) (`shovan`);
+
+
+ 
---
+++ 20_controlflow.js
@@ -0,0 +1,39 @@
+// //if
+// const isUserloggedin= true
+
+// if(2 !=3){
+// console.log("execute");
+// }
+// else{
+// console.log("not execute")
+// }
+// console.log ("sucessful");
+// <isUserloggedin, >, <=,>=, ==, !=, ===
+// const score = 200
+// if(score>100){
+//     let power ="fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+// const blance =10000
+
+// // if(blance>500) console.log("test2")
+// if(blance<500){
+//     console.log("less than 500");
+
+// }else if(blance<750){
+//     console.log("less than 750")
+// }else{
+//     console.log("less than 1200");
+// }
+const userLoggedin = true
+const debitCard= true
+
+if(userLoggedin && debitCard && 2==3){
+    console.log("allowto buy cource");
+}
+if(loggedInFromGoogle|| loggedInFromEmail){
+    console.log("user logged in");
+}
